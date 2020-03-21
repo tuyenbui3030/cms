c5130069f9467e56d343278ecd9518575e1aec18
tuyen <tuyen@ubuntu.ubuntu-domain>
2020-03-21 10:41:25 +0700

    Update CMS Login

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,7 +1,32 @@
 <?php include "../includes/db.php";?>
 <?php include "functions.php";?>
 <?php ob_start();?>
+<?php session_start();?>
+<?php
+    if(!isset($_SESSION['user_role']))
+    {
+        header("Location: ../index.php");
+    }
+    else
+    {
+        if($_SESSION['user_role'] !== 'admin')
+        {
+            if($_SESSION['user_role'] !== 'Admin')
+            {
+                header("Location: ../index.php");
+            }
+        }
+    }
+    
+    // if(isset($_SESSION['user_role']))
+    // {
+    //     if($_SESSION['user_role'] !== 'admin')
+    //     {
+    //         header("Location: ../index.php");
+    //     }
+    // }
 
+?>
 <!DOCTYPE html>
 <html lang="en">
 
