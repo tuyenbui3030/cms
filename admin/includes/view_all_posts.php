682bafc549de69dd33818a77a85981931d2f6951
tuyenbui3030 <tuyenbui3030@gmail.com>
2020-03-29 16:15:51 +0700

    CMS - update count views

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -111,9 +111,17 @@ if (isset($_POST['checkBoxArray'])) {
                 echo "<td>$post_status</td>";
                 echo "<td><img width='100' src='../images/$post_image' alt='image'></td>";
                 echo "<td>$post_tags</td>";
-                echo "<td>$post_comment_count</td>";
-                echo "<td>$post_date</td>";
 
+                $query = "SELECT * FROM comments WHERE comment_post_id = $post_id";
+                $send_comment_query = mysqli_query($connection, $query);
+                $count_comments = mysqli_num_rows($send_comment_query);
+
+                echo "<td>$count_comments</td>";
+
+
+
+
+                echo "<td>$post_date</td>";
                 echo "<td><a href='../post.php?p_id={$post_id}'>View post</a></td>";
                 echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                 echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -49,4 +49,4 @@ function loadUsersOnline() {
 
 setInterval(function () {
     loadUsersOnline();
-}, 500);
+}, 5000);
