ad83b152506baea36d5b305d3c0dbbd371a1728a
tuyenbui3030 <tuyenbui3030@gmail.com>
2020-04-01 19:13:31 +0700

    Update CMS - Send mail

--- contact.php
+++ contact.php
@@ -1,46 +1,39 @@
 <?php include "includes/db.php"; ?>
 <?php include "includes/header.php"; ?>
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
 if (isset($_POST['submit'])) {
-    $username   = $_POST['username'];
-    $email      = $_POST['email'];
-    $password   = $_POST['password'];
-    if (!empty($username) && !empty($email) && !empty($password)) {
-        $username   = mysqli_real_escape_string($connection, $username);
-        $email      = mysqli_real_escape_string($connection, $email);
-        $password   = mysqli_real_escape_string($connection, $password);
+//     $to         = "tuyenbui3030@gmail.com";
+//     $subject    = wordwrap($_POST['subject'], 70);
+//     $body       = $_POST['body'];
+//    echo $header     = $_POST['email'];
+//     mail($to,"$subject",$body);
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'devautosend@gmail.com';                     // SMTP username
+    $mail->Password   = 'meomeogaugau';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;      
 
-        $password   = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
-        // $query = "SELECT randSalt FROM users";
-        // $select_randsalt_query = mysqli_query($connection, $query);
-        // if (!$select_randsalt_query) {
-        //     die("Query Failed" . mysqli_error($connection));
-        // }
-        // $row = mysqli_fetch_array($select_randsalt_query);
-        // $salt = $row['randSalt'];
-        // $password = crypt($password, $salt);
+    //Recipients
+    $mail->setFrom($_POST['email'], $_POST['name']);
+    $mail->addAddress('Tuyenbui3030@gmail.com');                   // Name is optional
+    $mail->addReplyTo($_POST['email'],$_POST['name']);
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'From Submission: ' . $_POST['subject'];
+    $mail->Body    = $_POST['body'];
+    $mail->send();
+} 
 
-        $query = "INSERT INTO users(user_firstname, user_lastname, user_role, username, user_email, user_password, user_images) 
-    VALUES ('','','subscriber','{$username}','{$email}','{$password}', '')";
-        $register_user_query = mysqli_query($connection, $query);
-        if (!$register_user_query) {
-            die("QUERY FAILED " . mysqli_error($connection) . ' ' . mysqli_errno($connection));
-        }
-        $message = "Your Registration has been submitted";
-    }
-    else
-    { 
-        $message = "Fields cannot be empty";
-    }
-
-}
-else
-{
-    $message ="";
-}
 ?>
 
-<!-- Navigation -->
+<!-- Navigation --> 
 
 <?php include "includes/navigation.php"; ?>
 
@@ -54,22 +47,24 @@ else
                 <div class="col-xs-6 col-xs-offset-3">
                     <div class="form-wrap">
                         <h1>Contact</h1>
-                        <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
-                        <h6 class="text-center"><?php echo $message; ?></h6>    
-                            <div class="form-group">
-                                <label for="username" class="sr-only">username</label>
-                                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
+                        <form role="form" action="contact.php" method="post" id="login-form" autocomplete="off">
+                        <div class="form-group">
+                                <label for="name" class="sr-only">Name</label>
+                                <input type="text" name="name" id="name" class="form-control" placeholder="Enter your Name">
                             </div>
                             <div class="form-group">
                                 <label for="email" class="sr-only">Email</label>
-                                <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                                <input type="email" name="email" id="email" class="form-control" placeholder="Enter your Email">
                             </div>
                             <div class="form-group">
-                                <label for="password" class="sr-only">Password</label>
-                                <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                                <label for="subject" class="sr-only">Subject</label>
+                                <input type="text" name="subject" id="subject" class="form-control" placeholder="Enter your Subject">
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" name="body" id="body" cols="50" rows="10"></textarea>
                             </div>
 
-                            <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                            <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Submit">
                         </form>
 
                     </div>
